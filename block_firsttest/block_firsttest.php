780f50b4a58005bfe006f434d2c5dfabb7b38bb3
raveit65 <mate@raveit.de>
2022-03-02 22:32:55 +0100

    create block_firsttest.php

---
+++ block_firsttest/block_firsttest.php
@@ -0,0 +1,8 @@
+<?php
+class block_firsttest extends block_base {
+    public function init() {
+        $this->title = get_string('firsttest', 'block_firsttest');
+    }
+    // The PHP tag and the curly bracket for the class definition 
+    // will only be closed after there is another function added in the next section.
+}
