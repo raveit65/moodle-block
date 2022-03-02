e4d3ff5d36c1cebadc1b725d1fe434b115498514
raveit65 <mate@raveit.de>
2022-03-02 23:18:35 +0100

    add version.php

---
+++ block_firsttest/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2022030200;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->release    = '1.0.0';
+$plugin->component = 'block_firsttest';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
+$plugin->maturity   = MATURITY_STABLE;
