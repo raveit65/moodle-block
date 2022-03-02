7bc76bd8c2909757e5d91c0373cf7e7f1efc9566
raveit65 <mate@raveit.de>
2022-03-02 22:54:17 +0100

    add block_firsttest.php language file

---
+++ block_firsttest/lang/en/block_firsttest.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'First Test block';
+$string['firsttest'] = 'First Test';
+$string['firsttest:addinstance'] = 'Add a new first Test block';
+$string['firsttest:myaddinstance'] = 'Add a new first Test block to the My Moodle page';
